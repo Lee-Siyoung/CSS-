5bca62581dbda31c389829992d11bee5a9cbe7c4
Lee-Siyoung <tldud4861@gmail.com>
2023-10-24 18:14:23 +0900

    radiobutton

--- src/router.ts
+++ src/router.ts
@@ -3,12 +3,14 @@ import ColorPicker from "./components/ColorPicker.vue";
 import HomeView from "./components/HomeView.vue";
 import InfiniteSlideshow from "./components/InfiniteSlideshow.vue";
 import GlowingButton from "./components/GlowingButton.vue";
+import CustomRadioButton from "./components/CustomRadioButton.vue";
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", component: HomeView },
   { path: "/ColorPicker", component: ColorPicker },
   { path: "/InfiniteSlideshow", component: InfiniteSlideshow },
   { path: "/GlowingButton", component: GlowingButton },
+  { path: "/CustomRadioButton", component: CustomRadioButton },
 ];
 
 const router = createRouter({
